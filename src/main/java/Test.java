5aa2ed601dda3f64407337437687b3665c53ef60
Gerson2102 <71728860+Gerson2102@users.noreply.github.com>
2022-04-23 11:10:46 -0600

    Proyecto java 11

---
+++ src/main/java/Test.java
@@ -0,0 +1,21 @@
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+public class Test {
+
+    public static void main(String[] args) {
+        OrientDB orient = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig());
+        ODatabaseSession db = orient.open("test", "admin", "root");
+
+        ODocument animal = new ODocument("Client");
+        animal.field("name", "GERSON");
+        animal.field("location", "Guácimo");
+        animal.save();
+
+        //let's do something with this server!
+        db.close();
+        orient.close();
+    }
+}
